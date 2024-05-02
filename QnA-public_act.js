da42acfcca07cfb8b3b9b55a3e4f2ea60fe70352
leedayoung <leedayoung0601@gmail.com>
2024-05-03 00:30:53 +0900

    자료 열람 버튼 기능 수행 (추후 DB 연결 시 function 수정 + 시간 표시 필요함)

--- QnA-public_act.js
+++ QnA-public_act.js
@@ -1,9 +1,18 @@
+
+function navigateToPage(url) {
+    window.location.href = url;
+}
+
 function incrementLikes(button) {
     let likes = parseInt(button.innerText.match(/\d+/)[0]);
     likes += 1;
     button.innerHTML = `💜 ${likes}`;
 }
 
-function navigateToPage(url) {
-    window.location.href = url;
+/*DB 연결때 수정*/ 
+function showAnswer() {
+    document.querySelector('.open-button').style.display = 'none';
+    document.getElementById('additional-info').innerHTML = 'make all 명령은 모두 작성되어있습니다.';
+    document.getElementById('timestamp').innerHTML = '2024년 4월 2일 오전 4:01';
+    document.getElementById('timestamp').style.display = 'inline-block';
 }
