3a69dc6ba4e986914db98da65e3a0cb160ddbc90
leedayoung <leedayoung0601@gmail.com>
2024-05-15 17:24:10 +0900

    video 3번째 상세 페이지 생성 - 영상, pdf 다운가능하게 함. (추후 pdf, 영상 파일 첨부할 것)

--- video.js
+++ video.js
@@ -94,3 +94,16 @@ function updateVideoList(videos) {
     courseList.insertAdjacentHTML('beforeend', videoElement);
   });
 }
+
+var toggles = document.getElementsByClassName("accordion-toggle");
+        for (var i = 0; i < toggles.length; i++) {
+            toggles[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight){
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                } 
+            });
+        }
