1a5a84e56f1afe00c1d333b8894f897a7a37b05d
chaewon0401 <152391609+chaewon0401@users.noreply.github.com>
2024-05-23 01:55:28 +0900

    Add files via upload

--- graduate_school_main.js
+++ graduate_school_main.js
@@ -1,13 +1,11 @@
 var selectedBranch = null; // 선택된 분야의 정보를 저장할 변수
 var selectedRegion = null; // 선택된 지역의 정보를 저장할 변수
+var allData = []; // 전체 데이터를 저장할 배열
 
-// 클릭한 셀에 해당하는 분야 정보 저장하는 함수
 function selectBranch(branchId) {
-    // 이미 선택된 분야가 있다면 선택 해제
     if (selectedBranch === branchId) {
         selectedBranch = null;
     } else {
-        // 이전에 선택된 분야가 있다면 선택 해제
         if (selectedBranch !== null) {
             var prevSelectedCell = document.getElementById(selectedBranch);
             prevSelectedCell.classList.remove('selected');
@@ -17,13 +15,10 @@ function selectBranch(branchId) {
     updateCellStyle(branchId);
 }
 
-// 클릭한 셀에 해당하는 지역 정보 저장하는 함수
 function selectRegion(regionId) {
-    // 이미 선택된 지역이 있다면 선택 해제
     if (selectedRegion === regionId) {
         selectedRegion = null;
     } else {
-        // 이전에 선택된 지역이 있다면 선택 해제
         if (selectedRegion !== null) {
             var prevSelectedCell = document.getElementById(selectedRegion);
             prevSelectedCell.classList.remove('selected');
@@ -33,150 +28,96 @@ function selectRegion(regionId) {
     updateCellStyle(regionId);
 }
 
-// 셀의 스타일을 변경하는 함수
 function updateCellStyle(cellId) {
     var cell = document.getElementById(cellId);
-    cell.classList.toggle('selected');
+    if (cell) {
+        cell.classList.toggle('selected');
+    }
 }
 
-// 선택한 분야와 지역 정보를 표시하는 함수
 function showSelectedInfo() {
     var selectedInfoDiv = document.getElementById('selectedInfo');
 
-    if (selectedBranch === null && selectedRegion === null) {
+    if (selectedBranch === null || selectedRegion === null) {
         selectedInfoDiv.innerHTML = "분야와 지역을 모두 선택해주세요.";
     } else {
-        // 선택된 branch와 region 값을 가져와서 fetch 요청을 보냄
-        fetch(`graduate.php?branch=${selectedBranch}&region=${selectedRegion}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text(); // 응답을 텍스트로 파싱
-            })
-            .then(data => {
-                // 받은 데이터를 selectedInfoDiv에 표시
-                selectedInfoDiv.innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-                selectedInfoDiv.innerHTML = "데이터를 불러오는 도중 오류가 발생했습니다.";
+        // 필터링된 데이터를 가져오기
+        var filteredData = allData.filter(function(item) {
+            return item.branch === selectedBranch && item.region === selectedRegion;
+        });
+
+        // 필터링된 데이터를 HTML로 변환하여 표시
+        var html = '';
+        if (filteredData.length > 0) {
+            filteredData.forEach(function(item) {
+                html += `
+                    <div class="Info">
+                        <img src="profile.png" alt="Diss logo" class="profile2">
+                        <div class="Info_detail">
+                            <p><b>${item.name}</b></p>
+                            <h2><b>${item.school} ${item.major}</b></h2>
+                            <p>${item.region}</p>
+                            <p>Keywords: ${item.keyword1}, ${item.keyword2}, ${item.keyword3}</p>
+                        </div>
+                    </div>
+                `;
             });
+        } else {
+            html = "해당 정보를 찾을 수 없습니다.";
+        }
+        selectedInfoDiv.innerHTML = html;
         selectedInfoDiv.style.display = "flex";
     }
 }
 
-// 초기화 함수
 function resetSelection() {
-    // 선택된 분야 초기화
     if (selectedBranch !== null) {
         var prevSelectedBranchCell = document.getElementById(selectedBranch);
         prevSelectedBranchCell.classList.remove('selected');
         selectedBranch = null;
     }
 
-    // 선택된 지역 초기화
     if (selectedRegion !== null) {
         var prevSelectedRegionCell = document.getElementById(selectedRegion);
         prevSelectedRegionCell.classList.remove('selected');
         selectedRegion = null;
     }
 
-    // 선택된 정보 표시 초기화
     var selectedInfoDiv = document.getElementById('selectedInfo');
     selectedInfoDiv.style.display = "none";
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var branches = ['건축', '경영', '교육', '기계', '동물', '물리', '생명', '수학', '심리', '예체능', '의료', '전자', '조선', '화학', '환경', 'IT'];
+    branches.forEach(function(branch) {
+        var element = document.getElementById(branch);
+        if (element) {
+            element.addEventListener('click', function() {
+                selectBranch(branch);
+            });
+        }
+    });
 
-// 분야 테이블의 각 셀에 클릭 이벤트 추가하여 선택 정보 업데이트
-document.getElementById('건축').addEventListener('click', function() {
-    selectBranch('건축');
-});
+    var regions = ['강원', '경기', '경상', '서울', '인천', '전라', '제주', '충청'];
+    regions.forEach(function(region) {
+        var element = document.getElementById(region);
+        if (element) {
+            element.addEventListener('click', function() {
+                selectRegion(region);
+            });
+        }
+    });
 
-document.getElementById('경영').addEventListener('click', function() {
-    selectBranch('경영');
-});
-document.getElementById('교육').addEventListener('click', function() {
-    selectBranch('교육');
-});
+    document.getElementById('resetButton').addEventListener('click', resetSelection);
+    document.getElementById('searchButton').addEventListener('click', showSelectedInfo);
 
-document.getElementById('기계').addEventListener('click', function() {
-    selectBranch('기계');
-});
-document.getElementById('동물').addEventListener('click', function() {
-    selectBranch('동물');
-});
-
-document.getElementById('물리').addEventListener('click', function() {
-    selectBranch('물리');
-});
-document.getElementById('생명').addEventListener('click', function() {
-    selectBranch('생명');
-});
-
-document.getElementById('수학').addEventListener('click', function() {
-    selectBranch('수학');
-});
-document.getElementById('심리').addEventListener('click', function() {
-    selectBranch('심리');
-});
-
-document.getElementById('예체능').addEventListener('click', function() {
-    selectBranch('예체능');
-});
-document.getElementById('의료').addEventListener('click', function() {
-    selectBranch('의료');
-});
-
-document.getElementById('전자').addEventListener('click', function() {
-    selectBranch('전자');
-});
-document.getElementById('조선').addEventListener('click', function() {
-    selectBranch('조선');
-});
-
-document.getElementById('화학').addEventListener('click', function() {
-    selectBranch('화학');
-});
-document.getElementById('환경').addEventListener('click', function() {
-    selectBranch('환경');
-});
-
-document.getElementById('IT').addEventListener('click', function() {
-    selectBranch('IT');
-});
-
-// 지역 테이블의 각 셀에 클릭 이벤트 추가하여 선택 정보 업데이트
-document.getElementById('강원').addEventListener('click', function() {
-    selectRegion('강원');
-});
-
-document.getElementById('경기').addEventListener('click', function() {
-    selectRegion('경기');
-});
-document.getElementById('경상').addEventListener('click', function() {
-    selectRegion('경상');
-});
-
-document.getElementById('서울').addEventListener('click', function() {
-    selectRegion('서울');
-});
-document.getElementById('인천').addEventListener('click', function() {
-    selectRegion('인천');
-});
-
-document.getElementById('전라').addEventListener('click', function() {
-    selectRegion('전라');
-});
-document.getElementById('제주').addEventListener('click', function() {
-    selectRegion('제주');
-});
-
-document.getElementById('충청').addEventListener('click', function() {
-    selectRegion('충청');
-});
-
-// 초기화 버튼 클릭 이벤트 추가
-document.getElementById('resetButton').addEventListener('click', function() {
-    resetSelection();
+    // 페이지 로드 시 전체 데이터를 가져옵니다.
+    fetch('https://diss-graduate.azurewebsites.net/graduate.php')
+        .then(response => response.json())
+        .then(data => {
+            allData = data; // 전체 데이터를 저장
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 });
