62cde3af727fb2c7154ba40bb7ebd4b7dde29d44
chaewon0401 <152391609+chaewon0401@users.noreply.github.com>
2024-05-22 21:28:49 +0900

    Add files via upload

--- graduate.php
+++ graduate.php
@@ -1,59 +1,45 @@
 <?php
-// MySQL 서버 정보
-$host = "localhost";
-$user = "root";
-$pw = "fzFsCdG3yjA3[MBt";
-$dbName = "diss_graduate";
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
-// MySQL 데이터베이스에 연결
-$conn = new mysqli($host, $user, $pw, $dbName);
+// MSSQL 연결 정보
+$serverName = "dissgraduate-2.database.windows.net";
+$connectionOptions = array(
+    "Database" => "dissgraduate",
+    "Uid" => "DissPeople",
+    "PWD" => "cwbh0456@92"
+);
 
-// 연결 확인
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// MSSQL 연결
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    echo json_encode(array("error" => "Connection Error"));
+    die(print_r(sqlsrv_errors(), true));
 }
 
-// GET 요청으로부터 분야와 지역 가져오기
-$branch = $_GET['branch'];
-$region = $_GET['region'];
+// 데이터베이스에서 모든 데이터 선택
+$sql = "SELECT name, school, major, branch, region, keyword1, keyword2, keyword3 FROM dbo.Graduates";
+$stmt = sqlsrv_query($conn, $sql);
 
-// 데이터베이스에서 데이터 가져오기 (특정 분야와 지역에 해당하는 데이터만)
-$sql = "SELECT name, school, major, region 
-        FROM diss_graduateschool 
-        WHERE branch = ? AND region = ?
-        LIMIT 3";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $branch, $region);
-$stmt->execute();
-$result = $stmt->get_result();
-
-// 결과 출력
-if ($result->num_rows > 0) {
-    // 데이터를 HTML로 출력
-    echo '<select id="standard" name="standard">';
-    echo '<option value="popular">인기순</option>';
-    echo '<option value="recommend">추천순</option>';
-    echo '<option value="much">후기 많은순</option>';
-    echo '</select>';
-
-    while ($row = $result->fetch_assoc()) {
-        echo '<a href="DISS_graduate_school_detail.html">';
-        echo '<div class="Info">';
-        echo '<img src="profile.png" alt="Diss logo" class="profile2">';
-        echo '<div class="Info_detail">';
-        echo '<p><b>' . $row["name"] . '</b></p>';
-        echo '<h2><b>' . $row["school"] . ' ' . $row["major"] . '</b></h2>';
-        echo '<p>' . $row["region"] . '</p>';
-        echo '</div>';
-        echo '</div>';
-        echo '</a>';
-    }
-    echo  '<div class="button-more">' ;
-    echo ' <button type="button">더보기</button>';
-    echo '</div>';
-} else {
-    echo "해당 정보를 찾을 수 없습니다.";
+if ($stmt === false) {
+    echo json_encode(array("error" => "Query Error"));
+    die(print_r(sqlsrv_errors(), true));
 }
 
-// MySQL 연결 종료
-$conn->close();
+$data = array(); // 데이터를 저장할 배열 초기화
+
+while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+    // 각 행을 $data 배열에 추가
+    $data[] = $row;
+}
+
+// 배열을 JSON 형식으로 변환
+$json_data = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+// JSON 데이터를 출력
+echo $json_data;
+
+// MSSQL 연결 닫기
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
